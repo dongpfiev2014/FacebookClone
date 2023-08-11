fc08d04b48fe6ca46402fc758d34f7a4b0f813fc
dongpfiev2014 <dongpfiev2014@gmail.com>
2023-08-11 21:06:37 +0700

    Dong - fix profile.js

---
+++ js/profile.js
@@ -0,0 +1,13 @@
+// const searchInput = document.getElementById("searchInput");
+
+// searchInput.addEventListener("keypress", function (event) {
+//   if (event.key === "Enter") {
+//     performSearch();
+//   }
+// });
+
+// function performSearch() {
+//   const searchText = searchInput.value;
+//   // Thực hiện tìm kiếm hoặc xử lý logic tìm kiếm ở đây
+//   console.log("Searching for:", searchText);
+// }

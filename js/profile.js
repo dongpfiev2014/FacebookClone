b526e768bf52a324d2e456fa03659537784c8dd7
dongpfiev2014 <dongpfiev2014@gmail.com>
2023-08-14 04:57:24 +0700

    Đông - update html css

--- js/profile.js
+++ js/profile.js
@@ -11,3 +11,14 @@
 //   // Thực hiện tìm kiếm hoặc xử lý logic tìm kiếm ở đây
 //   console.log("Searching for:", searchText);
 // }
+
+// const fixedElement = document.getElementById("bold-text");
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 800) {
+//     // Điều kiện hiển thị sau khi kéo xuống 100px
+//     fixedElement.classList.remove("hidden");
+//   } else {
+//     fixedElement.classList.add("hidden");
+//   }
+// });

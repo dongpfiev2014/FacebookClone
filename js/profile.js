e1f306f011f50ea8371e0cf9e0e5d38c3d30ec66
dongpfiev2014 <dongpfiev2014@gmail.com>
2023-08-17 18:39:01 +0700

    Đông update html, css, js

--- js/profile.js
+++ js/profile.js
@@ -22,3 +22,28 @@
 //     fixedElement.classList.add("hidden");
 //   }
 // });
+
+//HÀM GIÚP THAY ĐỔI ICON CỦA BÌNH LUẬN Ở STATUS
+
+const commentBox = document.getElementById("comment-box");
+const commentButton = document.getElementById("comment-button");
+const commentIcon = document.getElementById("comment-icon");
+
+console.log(commentIcon);
+
+commentBox.addEventListener("focus", function () {
+  commentBox.classList.add("expanded");
+  commentButton.classList.remove("hidden");
+  commentIcon.classList.add("expanded-icon");
+  //   commentIcon.style.right = " ";
+  //   commentIcon.style.left = "20px";
+  //   commentIcon.style.bottom = "20px";
+});
+
+commentBox.addEventListener("blur", function () {
+  if (!commentBox.value.trim()) {
+    commentBox.classList.remove("expanded");
+    commentButton.classList.add("hidden");
+    commentIcon.classList.remove("expanded-icon");
+  }
+});
